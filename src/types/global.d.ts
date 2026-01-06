bafa793026b95fa38db7c76d6d22a651fe70e873
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2026-01-06 20:07:37 -0300

    feat(types): add TypeScript definitions for environment variables

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -38,7 +38,7 @@ declare module "*.svg" {
   export default src;
 }
 
-declare module '*.module.css' {
+declare module "*.module.css" {
   const styles: { [key: string]: string };
   export default styles;
 }
@@ -46,4 +46,11 @@ declare module '*.module.css' {
 declare module "*.scss" {
   const content: string;
   export default content;
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_API_URL?: string;
+    REACT_APP_REDIRECT_URL?: string;
+  }
 }
