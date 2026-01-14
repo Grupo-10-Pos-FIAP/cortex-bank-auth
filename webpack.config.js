1b1cd7017ffa8b64a9f8345918bfaf00cd4ccb3d
Gabrielle-96 <gabrielle.martiins.96@gmail.com>
2026-01-14 10:57:12 -0300

    feat deploy vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
-const { merge } = require("webpack-merge");
+const { mergeWithCustomize, customizeArray } = require("webpack-merge");
+const webpack = require("webpack");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
-const Dotenv = require("dotenv-webpack");
+const fs = require("fs");
+const path = require("path");
+
+// Carregar .env manualmente antes do singleSpaDefaults
+const envPath = path.resolve(__dirname, ".env");
+if (fs.existsSync(envPath)) {
+  const envFile = fs.readFileSync(envPath, "utf8");
+  envFile.split("\n").forEach((line) => {
+    const match = line.match(/^([^=:#]+)=(.*)$/);
+    if (match) {
+      const key = match[1].trim();
+      const value = match[2].trim().replace(/^["']|["']$/g, "");
+      if (!process.env[key]) {
+        process.env[key] = value;
+      }
+    }
+  });
+}
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -10,12 +28,28 @@ module.exports = (webpackConfigEnv, argv) => {
     argv,
   });
 
-  return merge(defaultConfig, {
+  return mergeWithCustomize({
+    customizeArray: customizeArray({
+      // Substitui DefinePlugin existente ao invés de adicionar um novo
+      plugins: (basePlugins, newPlugins) => {
+        const filteredBasePlugins = basePlugins.filter(
+          (plugin) => !(plugin instanceof webpack.DefinePlugin)
+        );
+        return [...filteredBasePlugins, ...newPlugins];
+      },
+    }),
+  })(defaultConfig, {
     plugins: [
-      new Dotenv({
-        path: "./.env",
-        safe: false,
-        systemvars: true,
+      new webpack.DefinePlugin({
+        "process.env.REACT_APP_API_URL": JSON.stringify(
+          process.env.REACT_APP_API_URL || "http://localhost:3000"
+        ),
+        "process.env.REACT_APP_REDIRECT_URL": JSON.stringify(
+          process.env.REACT_APP_REDIRECT_URL || "http://localhost:3000/dashboard"
+        ),
+        "process.env.NODE_ENV": JSON.stringify(
+          process.env.NODE_ENV || webpackConfigEnv.mode || "development"
+        ),
       }),
     ],
   });
